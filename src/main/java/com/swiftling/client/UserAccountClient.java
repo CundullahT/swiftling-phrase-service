aeb1a569d429295a417ac7ffe3278c0e5b11afb3
CundullahT-CT <cterzioglu@cybertekschool.com>
2025-05-31 19:36:38 +0200

    removes email passing to getNonCompletedCountByAssignedManager client method

--- src/main/java/com/swiftling/client/UserAccountClient.java
+++ src/main/java/com/swiftling/client/UserAccountClient.java
@@ -4,12 +4,11 @@ import com.swiftling.dto.UserAccountResponseDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(value = "swiftling-user-service")
 public interface UserAccountClient {
 
     @GetMapping("/api/v1/account/get-external-id")
-    ResponseEntity<UserAccountResponseDTO> getNonCompletedCountByAssignedManager(@RequestParam("email") String email);
+    ResponseEntity<UserAccountResponseDTO> getNonCompletedCountByAssignedManager();
 
 }
