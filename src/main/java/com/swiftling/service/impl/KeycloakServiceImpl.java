b1af0227616ddc9265975fbff104c5c93486c827
CundullahT-CT <cterzioglu@cybertekschool.com>
2025-06-17 11:29:32 +0200

    fixes Keycloak service methods

--- src/main/java/com/swiftling/service/impl/KeycloakServiceImpl.java
+++ src/main/java/com/swiftling/service/impl/KeycloakServiceImpl.java
@@ -7,8 +7,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Service;
 
-import java.util.Map;
-
 @Service
 public class KeycloakServiceImpl implements KeycloakService {
 
